8a8502d4ddd858b1c681a6bbfd2b8d0b4851dd28
MoreFunGuy <ximengsunyichen@sina.com>
2015-08-12 17:19:24 +0800

    update 0812(2)

---
+++ scripts/index_another.js
@@ -0,0 +1,32 @@
+//给head加下拉菜单
+var list_parent = document.getElementById("list_parent");
+var list_head = document.querySelector("#list_head");
+var list_body = document.querySelector("#list_body");
+list_parent.onmouseover = function(){
+	list_body.style.display = "block";
+	
+};
+list_parent.onmouseout = function(){
+	list_body.style.display = "none";
+};
+list_head.onmouseover = function(){
+	list_head.style.boxShadow = "1px 1px 2px #152840 inset";
+}
+list_head.onmouseout = function(){
+	list_head.style.boxShadow = "";
+}
+
+var lbp = document.querySelectorAll("#list_body .list_body_parent");
+for(var i=0;i<lbp.length;i++){
+	lbp[i].onmouseover = function(){
+		this.childNodes[3].style.display = "block";
+	};
+	lbp[i].onmouseout = function(){
+		this.childNodes[3].style.display = "none";
+	};
+}
+
+
+
+
+
